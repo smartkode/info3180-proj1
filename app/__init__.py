e763f055cf7b87bceeb220060942a494f1a3418b
Craig <craigh89@yahoo.com>
2015-05-06 09:15:35 -0500

    db info

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postgres'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wrzoerdkpbgaeh:ExE09bAKJpCL0zrZbMdoKfL77c@ec2-23-21-96-129.compute-1.amazonaws.com:5432/d466s8ob9kv9av'
 app.config['SECRET_KEY']='sjddjhdgjdshdghjsvdn'
 
 db = SQLAlchemy(app)
